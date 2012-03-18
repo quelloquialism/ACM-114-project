b1b270ab16704e9ff2aae642f261cbe6785e9d97
Nathan <quelloquialism@gmail.com>
2012-03-18 02:13:26 -0700

    threaded dijkstra, prim

--- threaded/dijkstra.cpp
+++ threaded/dijkstra.cpp
@@ -1,38 +1,93 @@
+#include <cmath>
 #include <iostream>
 #include <limits>
+#include <pthread.h>
 #include <queue>
 
 using namespace std;
 
-double* dijkstra(int vertices, double* adjmat, int source) {
+struct CommonData {
+  int vertices;
+  int vis_count;
   priority_queue<pair<double, int> > q;
-  double* dist = new double[vertices];
-  bool vis[vertices];
-  
-  for (int i = 0; i < vertices; i++) {
-    dist[i] = numeric_limits<double>::infinity();
-    vis[i] = false;
-  }
-  dist[source] = 0;
-  q.push(make_pair(0, source));
+  double* dist;
+  double* adjmat;
+  pthread_mutex_t* vis_count_mutex;
+  pthread_mutex_t* q_mutex;
+};
 
-  while (!q.empty()) {
-    pair<double, int> curr = q.top();
-    int vert = curr.second;
-    q.pop();
+void* dijkstra_worker(void* ptr) {
+  CommonData* cdata = (CommonData*)ptr;
+  int vertices = cdata->vertices;
+  double* dist = cdata->dist;
+  double* adjmat = cdata->adjmat;
 
-    if (!vis[vert]) {
-      vis[vert] = true;
+  while (!cdata->q.empty()) {
+    // lock q
+    pthread_mutex_lock(cdata->q_mutex);
+    bool valid = !cdata->q.empty();
+    pair<double, int> curr;
+    int vert;
+    if (valid) {
+      curr = cdata->q.top();
+      vert = curr.second;
+      cdata->q.pop();
+    }
+    // unlock q
+    pthread_mutex_unlock(cdata->q_mutex);
+
+    if (valid && -curr.first < dist[vert]) {
+      dist[vert] = abs(curr.first);
+      // lock vis_count
+      pthread_mutex_lock(cdata->q_mutex);
+      pthread_mutex_lock(cdata->vis_count_mutex);
+      cdata->vis_count++;
+      // unlock vis_count
+      pthread_mutex_unlock(cdata->vis_count_mutex);
+
       for (int i = 0; i < vertices; i++) {
-        if (!vis[i] && dist[vert] + adjmat[vert * vertices + i] < dist[i]) {
-          dist[i] = dist[vert] + adjmat[vert * vertices + i];
-          q.push(make_pair(-dist[i], i));
+        // lock q
+        if (dist[vert] + adjmat[vert * vertices + i] < dist[i]) {
+          cdata->q.push(make_pair(
+              -dist[vert] - adjmat[vert * vertices + i], i
+          ));
         }
+        // unlock q
       }
+      pthread_mutex_unlock(cdata->q_mutex);
     }
   }
 
-  return dist;
+  return 0;
+}
+
+double* dijkstra(int vertices, double* adjmat, int source, int threads) {
+  CommonData cdata;
+  pthread_mutex_t vis_count_mutex;
+  pthread_mutex_init(&vis_count_mutex, 0);
+  pthread_mutex_t q_mutex;
+  pthread_mutex_init(&q_mutex, 0);
+  cdata.vertices = vertices;
+  cdata.vis_count = 0;
+  cdata.dist = new double[vertices];
+  cdata.adjmat = adjmat;
+  cdata.vis_count_mutex = &vis_count_mutex;
+  cdata.q_mutex = &q_mutex;
+  pthread_t th[threads];
+
+  for (int i = 0; i < vertices; i++) {
+    cdata.dist[i] = numeric_limits<double>::infinity();
+  }
+  cdata.q.push(make_pair(0, source));
+
+  for (int i = 0; i < threads; i++) {
+    pthread_create(&th[i], NULL, dijkstra_worker, (void*)&cdata);
+  }
+  for (int i = 0; i < threads; i++) {
+    pthread_join(th[i], NULL);
+  }
+
+  return cdata.dist;
 }
 
 int main() {
@@ -43,7 +98,7 @@ int main() {
     cin >> adjmat[i];
   }
 
-  double* dist = dijkstra(vertices, adjmat, source);
+  double* dist = dijkstra(vertices, adjmat, source, 4);
   for (int i = 0; i < vertices; i++) {
     cout << dist[i];
     if (i == vertices - 1) {
--- threaded/prim.cpp
+++ threaded/prim.cpp
@@ -1,45 +1,102 @@
+#include <cmath>
 #include <iostream>
 #include <limits>
+#include <pthread.h>
 #include <queue>
 
 using namespace std;
 
-vector<pair<int, int> > prim(int vertices, double* adjmat) {
+struct CommonData {
+  int vertices;
+  int vis_count;
   vector<pair<int, int> > mst;
   priority_queue<pair<double, int> > q;
-  double* dist = new double[vertices];
-  bool vis[vertices];
-  int parent[vertices];
-  
-  for (int i = 0; i < vertices; i++) {
-    dist[i] = numeric_limits<double>::infinity();
-    vis[i] = false;
-  }
-  dist[0] = 0;
-  parent[0] = -1;
-  q.push(make_pair(0, 0));
+  double* dist;
+  double* adjmat;
+  int* parent;
+  pthread_mutex_t* vis_count_mutex;
+  pthread_mutex_t* q_mutex;
+};
 
-  while (!q.empty()) {
-    pair<double, int> curr = q.top();
-    int vert = curr.second;
-    q.pop();
+void* prim_worker(void* ptr) {
+  CommonData* cdata = (CommonData*)ptr;
+  int vertices = cdata->vertices;
+  double* dist = cdata->dist;
+  double* adjmat = cdata->adjmat;
+  int* parent = cdata->parent;
 
-    if (!vis[vert]) {
-      vis[vert] = true;
+  while (!cdata->q.empty()) {
+    // lock q
+    pthread_mutex_lock(cdata->q_mutex);
+    bool valid = !cdata->q.empty();
+    pair<double, int> curr;
+    int vert;
+    if (valid) {
+      curr = cdata->q.top();
+      vert = curr.second;
+      cdata->q.pop();
+    }
+    // unlock q
+    pthread_mutex_unlock(cdata->q_mutex);
+
+    if (valid && -curr.first < dist[vert]) {
+      dist[vert] = abs(curr.first);
       if (parent[vert] != -1) {
-        mst.push_back(make_pair(parent[vert], vert));
+        cdata->mst.push_back(make_pair(parent[vert], vert));
       }
+      pthread_mutex_lock(cdata->q_mutex);
+      // lock vis_count
+      pthread_mutex_lock(cdata->vis_count_mutex);
+      cdata->vis_count++;
+      // unlock vis_count
+      pthread_mutex_unlock(cdata->vis_count_mutex);
+
       for (int i = 0; i < vertices; i++) {
-        if (!vis[i] && dist[vert] + adjmat[vert * vertices + i] < dist[i]) {
+        if (dist[vert] + adjmat[vert * vertices + i] < dist[i]) {
           parent[i] = vert;
-          dist[i] = dist[vert] + adjmat[vert * vertices + i];
-          q.push(make_pair(-dist[i], i));
+          cdata->q.push(make_pair(
+              -dist[vert] - adjmat[vert * vertices + i], i
+          ));
         }
       }
+      pthread_mutex_unlock(cdata->q_mutex);
     }
   }
 
-  return mst;
+  return 0;
+}
+
+vector<pair<int, int> > prim(int vertices, double* adjmat, int threads) {
+  vector<pair<int, int> > mst;
+  CommonData cdata;
+  pthread_mutex_t vis_count_mutex;
+  pthread_mutex_init(&vis_count_mutex, 0);
+  pthread_mutex_t q_mutex;
+  pthread_mutex_init(&q_mutex, 0);
+  cdata.mst = mst;
+  cdata.vertices = vertices;
+  cdata.vis_count = 0;
+  cdata.dist = new double[vertices];
+  cdata.parent = new int[vertices];
+  cdata.parent[0] = -1;
+  cdata.adjmat = adjmat;
+  cdata.vis_count_mutex = &vis_count_mutex;
+  cdata.q_mutex = &q_mutex;
+  pthread_t th[threads];
+
+  for (int i = 0; i < vertices; i++) {
+    cdata.dist[i] = numeric_limits<double>::infinity();
+  }
+  cdata.q.push(make_pair(0, 0));
+
+  for (int i = 0; i < threads; i++) {
+    pthread_create(&th[i], NULL, prim_worker, (void*)&cdata);
+  }
+  for (int i = 0; i < threads; i++) {
+    pthread_join(th[i], NULL);
+  }
+
+  return cdata.mst;
 }
 
 int main() {
@@ -50,7 +107,7 @@ int main() {
     cin >> adjmat[i];
   }
 
-  vector<pair<int, int> > mst = prim(vertices, adjmat);
+  vector<pair<int, int> > mst = prim(vertices, adjmat, 4);
 
   for (size_t i = 0; i < mst.size(); i++) {
     cout << mst[i].first << "-" << mst[i].second;

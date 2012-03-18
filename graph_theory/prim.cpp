090240135bf2259dbbc15c2747afaf1ad899c2e6
Nathan <quelloquialism@gmail.com>
2012-03-18 02:57:54 -0700

    it said README but it was blank, so i unblanked it

--- graph_theory/prim.cpp
+++ graph_theory/prim.cpp
@@ -99,6 +99,44 @@ vector<pair<int, int> > prim(int vertices, double* adjmat, int threads) {
   return cdata.mst;
 }
 
+vector<pair<int, int> > _prim(int vertices, double* adjmat) {
+  vector<pair<int, int> > mst;
+  priority_queue<pair<double, int> > q;
+  double* dist = new double[vertices];
+  bool vis[vertices];
+  int parent[vertices];
+  
+  for (int i = 0; i < vertices; i++) {
+    dist[i] = numeric_limits<double>::infinity();
+    vis[i] = false;
+  }
+  dist[0] = 0;
+  parent[0] = -1;
+  q.push(make_pair(0, 0));
+
+  while (!q.empty()) {
+    pair<double, int> curr = q.top();
+    int vert = curr.second;
+    q.pop();
+
+    if (!vis[vert]) {
+      vis[vert] = true;
+      if (parent[vert] != -1) {
+        mst.push_back(make_pair(parent[vert], vert));
+      }
+      for (int i = 0; i < vertices; i++) {
+        if (!vis[i] && dist[vert] + adjmat[vert * vertices + i] < dist[i]) {
+          parent[i] = vert;
+          dist[i] = dist[vert] + adjmat[vert * vertices + i];
+          q.push(make_pair(-dist[i], i));
+        }
+      }
+    }
+  }
+
+  return mst;
+}
+
 int main() {
   int vertices;
   cin >> vertices;
@@ -107,7 +145,7 @@ int main() {
     cin >> adjmat[i];
   }
 
-  vector<pair<int, int> > mst = prim(vertices, adjmat, 4);
+  vector<pair<int, int> > mst = _prim(vertices, adjmat);
 
   for (size_t i = 0; i < mst.size(); i++) {
     cout << mst[i].first << "-" << mst[i].second;
